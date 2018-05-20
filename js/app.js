f3ac8b93017baeb5ec190ca149863bca6664e32c
Bella Dear <arabelladear@hotmail.co.uk>
2018-05-20 21:56:57 +0100

    check for match function, input and sumbit added

--- js/app.js
+++ js/app.js
@@ -8,8 +8,22 @@ $(() => {
   const $target2 = $('#2');
   const $target3 = $('#3');
   const $target4 = $('#4');
+  const $targets = $('.targets');
 
+  const $submit = $('#submit');
+  const $input = $('#name-input');
+  const $name = $('#name');
 
+  //enter name
+
+  $targets.hide();
+
+  $submit.on('click', function(){
+    $targets.show();
+    $submit.hide();
+    $input.hide();
+    $name.html($input.val());
+  });
 
   //fireball function
 
@@ -27,9 +41,36 @@ $(() => {
       const $ballOffset = $ball.offset();
       const $targetOffset = target.offset();
 
-      if ($ballOffset.top < $targetOffset.top + target.height()) {
-        console.log('collision detected');
+      function checkForCollison(){
+        if ($ballOffset.top < $targetOffset.top + target.height() &&
+            $ballOffset.top +$ball.height()) {
+          console.log('collision detected');
+        }
       }
+
+      //function checkForMatch
+
+      $(document).on('keydown', function(e){
+        switch(e.which){
+          case 65:
+            if ($ballOffset.top < $targetOffset.top + target.height() &&
+              $ballOffset.top + $ball.height() > $targetOffset.top) {
+              console.log('collision detected');
+            }
+            break;
+          case 83:
+            // checkForCollison();
+            break;
+          case 68:
+            // checkForCollison();
+            break;
+          case 70:
+            // checkForCollison();
+            break;
+        }
+      });
+
+      //removal and clear interval
 
       if ($ball.position().top+$ball.height() === 0) {
         $ball.remove();
@@ -41,44 +82,46 @@ $(() => {
 
 
   //intervals
+  $submit.on('click', function(){
+    setInterval(function(){
+      for (let i = 0; i < timings1.length; i++) {
+        setTimeout(function(){
+          fireBall($target1);
+        }, timings1[i]);
+      }
+    }, 10000);
 
-  setInterval(function(){
-    for (let i = 0; i < timings1.length; i++) {
-      setTimeout(function(){
-        fireBall($target1);
-      }, timings1[i]);
-    }
-  }, 1000);
+    setInterval(function(){
+      for (let i = 0; i < timings2.length; i++) {
+        setTimeout(function(){
+          fireBall($target2);
+        }, timings2[i]);
+      }
+    }, 10000);
 
-  setInterval(function(){
-    for (let i = 0; i < timings2.length; i++) {
-      setTimeout(function(){
-        fireBall($target2);
-      }, timings2[i]);
-    }
-  }, 10000);
+    setInterval(function(){
+      for (let i = 0; i < timings3.length; i++) {
+        setTimeout(function(){
+          fireBall($target3);
+        }, timings3[i]);
+      }
+    }, 10000);
 
-  setInterval(function(){
-    for (let i = 0; i < timings3.length; i++) {
-      setTimeout(function(){
-        fireBall($target3);
-      }, timings3[i]);
-    }
-  }, 10000);
-
-  setInterval(function(){
-    for (let i = 0; i < timings4.length; i++) {
-      setTimeout(function(){
-        fireBall($target4);
-      }, timings4[i]);
-    }
-  }, 10000);
-
+    setInterval(function(){
+      for (let i = 0; i < timings4.length; i++) {
+        setTimeout(function(){
+          fireBall($target4);
+        }, timings4[i]);
+      }
+    }, 10000);
+  });
 
   //function centre collision
 
 
-  //function checkForMatch
+
+
+
 
   //function scoreIncrease
 
