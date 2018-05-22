9caf2d6667b75968a2fd9b09ca1e2a30213bf2e9
Bella Dear <arabelladear@hotmail.co.uk>
2018-05-22 17:33:47 +0100

    two player completed, jump to next input field working

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 $(() => {
+  //VARIABLES
+
   const timings1 = [329.67033, 659.340659, 1978.02198, 3296.7033, 5274.72527];
   const timings2 = [1318.68132, 1978.02198, 3296.7033, 4615.38462, 6593.40659];
   const timings3 = [3956.04396, 4615.38462, 5934.06593];
@@ -8,7 +10,6 @@ $(() => {
   const $target2 = $('#2');
   const $target3 = $('#3');
   const $target4 = $('#4');
-  const $targets = $('.targets');
 
   const $target5 = $('#5');
   const $target6 = $('#6');
@@ -24,7 +25,6 @@ $(() => {
   const $1playerButton = $('#player-1');
   const $2playerButton = $('#player-2');
 
-  const $comments = $('.comments');
   const $comment1 = $('#comment1');
   const $comment2 = $('#comment2');
   const $comment3 = $('#comment3');
@@ -49,8 +49,8 @@ $(() => {
   const $displayName2 = $('#name-2');
 
   const $chooseKeys = $('.choose-keys');
-  const $1PKeys = $('.player-1-keys');
   const $2PKeys = $('.player-2-keys');
+  const $keyInputs = $('.key-inputs');
   const $key1 = $('#key1');
   const $key2 = $('#key2');
   const $key3 = $('#key3');
@@ -62,10 +62,12 @@ $(() => {
   const $submitKeys = $('#play');
   const $submitKeys2 = $('#play-2');
   const $audio = $('#audio');
-  const $scoreboard = $('.scoreboard');
-  const $score = $('.score');
+  const $scoreboards = $('.scoreboards');
+  const $scoreboard2 = $('.scoreboard2');
+  const $score = $('.score1');
+  const $score2 = $('.score2');
   let score = 0;
-
+  let score2 = 0;
 
   //AUDIO
 
@@ -78,7 +80,7 @@ $(() => {
 
   $buttons.show();
   $1and2PTargets.hide();
-  $scoreboard.hide();
+  $scoreboards.hide();
   $chooseKeys.hide();
   $enterName.hide();
   $enterName2.hide();
@@ -101,15 +103,22 @@ $(() => {
     $enterName.remove();
   });
 
-  // $key1.keydown(function(){
-  //   if($key1.val().length === this.maxlength) {
-  //     $key2.focus();
-  //   }
-  // });
-
   $input.keydown(function(e){
     if(e.keyCode === 13){
       $submitName.click();
+    }
+  });
+
+  $keyInputs.keyup(function(){
+    console.log($(this).length);
+    if($(this).val().length === 1){
+      $(this).next().focus();
+    }
+  });
+
+  $keyInputs.keydown(function(e){
+    if(e.keyCode === 13){
+      $submitKeys.click();
     }
   });
 
@@ -120,14 +129,9 @@ $(() => {
     $chooseKeys.remove();
     $1and2PTargets.show();
     $2PTargets.hide();
-    $scoreboard.show();
+    $scoreboards.show();
+    $scoreboard2.hide();
     $displayName.html($input.val());
-  });
-
-  $chooseKeys.keydown(function(e){
-    if(e.keyCode === 13){
-      $submitKeys.click();
-    }
   });
 
   //FIREBALL
@@ -143,7 +147,6 @@ $(() => {
       $ball.css('top', '-=1px');
 
       if ($ball.position().top + $ball.height() === 0) {
-        increaseScoreBy(-1);
         $ball.remove();
         $ballsArray = $ballsArray.filter(function(ball) {
           return !(ball === $ball);
@@ -153,52 +156,41 @@ $(() => {
     }, 5);
   }
 
-  //COLLISION DETECTION
+  //COLLISION
 
-  function collisionDetection(target, comment){
+  function ballCollides(i, target, accuracy){
+    const collidesAbove = $ballsArray[i].offset().top < target.offset().top + target.height()/accuracy;
+    const collidesBelow = $ballsArray[i].offset().top + $ballsArray[i].height()/accuracy > target.offset().top;
+    const collidesLeft = $ballsArray[i].offset().left < target.offset().left + target.width()/accuracy;
+    const collidesRight = $ballsArray[i].offset().left + $ballsArray[i].width()/accuracy > target.offset().left;
+    return collidesAbove && collidesBelow && collidesLeft && collidesRight;
+  }
+
+  //EFFECT OF COLLISION
+
+  function collisionEffect(ballNumber, target, playerNumber, score, commentElement){
+    increaseScoreBy(playerNumber, score);
+    $ballsArray[ballNumber].remove();
+    target.css('box-shadow', '0 0 20px #f5f3ce');
+    setTimeout(function(){
+      target.css('box-shadow', 'none');
+    }, 500);
+    commentElement.text('Perfect!');
+    setTimeout(function(){
+      commentElement.fadeOut();
+    }, 1000);
+  }
+
+  //COLLSION DETECTION
+
+  function detectCollision(target, commentElement, playerNumber){
     for (let i = 0; i < $ballsArray.length; i++){
-      if ($ballsArray[i].offset().top < target.offset().top + target.height()/3 &&
-      $ballsArray[i].offset().top + $ballsArray[i].height()/3 > target.offset().top &&
-      $ballsArray[i].offset().left < target.offset().left + target.width()/3 &&
-      $ballsArray[i].offset().left + $ballsArray[i].width()/3 > target.offset().left) {
-        increaseScoreBy(3);
-        $ballsArray[i].remove();
-        target.css('box-shadow', '0 0 20px #f5f3ce');
-        setTimeout(function(){
-          target.css('box-shadow', 'none');
-        }, 500);
-        comment.text('Perfect!');
-        setTimeout(function(){
-          comment.fadeOut();
-        }, 1000);
-      } else if ($ballsArray[i].offset().top < target.offset().top + target.height()/2 &&
-      $ballsArray[i].offset().top + $ballsArray[i].height()/2 > target.offset().top &&
-      $ballsArray[i].offset().left < target.offset().left + target.width()/2 &&
-      $ballsArray[i].offset().left + $ballsArray[i].width()/2 > target.offset().left) {
-        increaseScoreBy(2);
-        $ballsArray[i].remove();
-        target.css('box-shadow', '0 0 10px #f5f3ce');
-        setTimeout(function(){
-          target.css('box-shadow', 'none');
-        }, 500);
-        comment.text('Good!');
-        setTimeout(function(){
-          comment.fadeOut();
-        }, 1000);
-      } else if ($ballsArray[i].offset().top < target.offset().top + target.height() &&
-      $ballsArray[i].offset().top + $ballsArray[i].height() > target.offset().top &&
-      $ballsArray[i].offset().left < target.offset().left + target.width() &&
-      $ballsArray[i].offset().left + $ballsArray[i].width() > target.offset().left){
-        increaseScoreBy(1);
-        $ballsArray[i].remove();
-        target.css('box-shadow', '0 0 5px #f5f3ce');
-        setTimeout(function(){
-          target.css('box-shadow', 'none');
-        }, 500);
-        comment.text('Ok!');
-        setTimeout(function(){
-          comment.fadeOut();
-        }, 1000);
+      if (ballCollides(i, target, 3)) {
+        collisionEffect(i, target, playerNumber, 3, commentElement);
+      } else if (ballCollides(i, target, 2)) {
+        collisionEffect(i, target, playerNumber, 2, commentElement);
+      } else if (ballCollides(i, target, 1)){
+        collisionEffect(i, target, playerNumber, 1, commentElement);
       }
     }
   }
@@ -214,28 +206,28 @@ $(() => {
   $(document).on('keydown', function(e){
     switch(e.which){
       case getKeyValue($key1):
-        collisionDetection($target1, $comment1);
+        detectCollision($target1, $comment1, 1);
         break;
       case getKeyValue($key2):
-        collisionDetection($target2, $comment2);
+        detectCollision($target2, $comment2, 1);
         break;
       case getKeyValue($key3):
-        collisionDetection($target3, $comment3);
+        detectCollision($target3, $comment3, 1);
         break;
       case getKeyValue($key4):
-        collisionDetection($target4, $comment4);
+        detectCollision($target4, $comment4, 1);
         break;
       case getKeyValue($key5):
-        collisionDetection($target5, $comment5);
+        detectCollision($target5, $comment5, 2);
         break;
       case getKeyValue($key6):
-        collisionDetection($target6, $comment6);
+        detectCollision($target6, $comment6, 2);
         break;
       case getKeyValue($key7):
-        collisionDetection($target7, $comment7);
+        detectCollision($target7, $comment7, 2);
         break;
       case getKeyValue($key8):
-        collisionDetection($target8, $comment8);
+        detectCollision($target8, $comment8, 2);
     }
   });
 
@@ -258,12 +250,16 @@ $(() => {
     ballIntervals(timings4, $target4, 7252.74725);
   });
 
-
   //SCORE
 
-  function increaseScoreBy(number){
-    score += number;
-    $score.text(score);
+  function increaseScoreBy(playerNumber, number){
+    if (playerNumber === 1){
+      score += number;
+      $score.text(score);
+    } else if (playerNumber === 2){
+      score2 += number;
+      $score2.text(score2);
+    }
   }
 
   //2 PLAYER
@@ -275,27 +271,37 @@ $(() => {
     $input.focus();
 
     $submitName.on('click', function(){
-      $input2.show().focus();
       $enterName2.show();
+      $input2.focus();
       $chooseKeys.hide();
 
       $submitName2.on('click', function(){
         $chooseKeys.show();
-        $submitKeys.hide();
+        $key1.focus();
         $2PKeys.show();
+        $submitKeys.hide();
         $enterName2.remove();
         $player1Name.text($input.val());
         $player2Name.text($input2.val());
+
+      });
+
+      $enterName2.keydown(function(e){
+        if(e.keyCode === 13){
+          $submitName2.click();
+        }
       });
     });
   });
 
   $submitKeys2.on('click', function(){
+    sound();
     $chooseKeys.remove();
     $1and2PTargets.show();
     $1and2PComments.show();
-    $displayName.text($input.val());
-    $displayName2.text($input2.val());
+    $scoreboards.show();
+    $displayName.html($input.val());
+    $displayName2.html($input2.val());
 
     ballIntervals(timings1, $target1, 7252.74725);
     ballIntervals(timings2, $target2, 7252.74725);
@@ -306,6 +312,5 @@ $(() => {
     ballIntervals(timings3, $target7, 7252.74725);
     ballIntervals(timings4, $target8, 7252.74725);
   });
-
 
 });
